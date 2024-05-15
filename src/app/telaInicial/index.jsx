07d7729fa5c65582fb1132ec18422fc407e48230
Diogo_Mazeika <diogo.camargo@hspsoftware.com.br>
2024-05-15 17:28:55 -0300

    loucuras

--- src/app/telaInicial/index.jsx
+++ src/app/telaInicial/index.jsx
@@ -1,55 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Row } from 'reactstrap';
 import { Colxx } from '../../components/generic/reactstrap';
 import ReactSwiper from '../../components/generic/swiper';
-import { useNavigate } from 'react-router-dom';
 
 export default function TelaInicial() {
   const history = useNavigate();
+
+  const [homeArquivos, setHomeArquivos] = useState([]);
+
+  useEffect(() => {
+    const aux = [{ id: 1 }];
+    setHomeArquivos([...aux]);
+  }, []);
+
   return (
     <Colxx>
       <Row>
         <Colxx xxs="5">
           <ReactSwiper>
-            <div
-              className="c-pointer w-100 h-100"
-              onClick={() => {
-                history('app/arquivo');
-              }}
-            >
-              img1
-            </div>
-            <div
-              className="c-pointer w-100 h-100"
-              onClick={() => {
-                history('app/arquivo');
-              }}
-            >
-              img2
-            </div>
-            <div
-              className="c-pointer w-100 h-100"
-              onClick={() => {
-                history('app/arquivo');
-              }}
-            >
-              img3
-            </div>
-            <div
-              className="c-pointer w-100 h-100"
-              onClick={() => {
-                history('aasdsad');
-              }}
-            >
-              img4
-            </div>
-            <div
-              className="c-pointer w-100 h-100"
-              onClick={() => {
-                history('aasdsad');
-              }}
-            >
-              img5
-            </div>
+            {homeArquivos.map(({ id }) => {
+              return (
+                <div
+                  key={id}
+                  className="c-pointer w-100 h-100"
+                  onClick={() => {
+                    history('app/arquivo');
+                  }}
+                >
+                  img
+                </div>
+              );
+            })}
           </ReactSwiper>
         </Colxx>
         <Colxx xxs="7">alguma coisa aqui</Colxx>
--- src/components/core/topbar.jsx
+++ src/components/core/topbar.jsx
@@ -1,34 +1,37 @@
-import { useState } from "react";
-import { Row } from "reactstrap";
-import { Colxx } from "../generic/reactstrap";
-import RcSwitch from "../generic/rcSwitch";
-import Svg from "../generic/svg";
+import { useEffect, useState } from 'react';
+import { Row } from 'reactstrap';
+import { Colxx } from '../generic/reactstrap';
+import RcSwitch from '../generic/rcSwitch';
+import Svg from '../generic/svg';
 
 export default function Topbar() {
   const [switchTheme, setSwitchTheme] = useState(false);
+
+  useEffect(() => {
+    document.body.className = switchTheme ? 'dark-mode' : '';
+  }, [switchTheme]);
+
   return (
     <div id="main-topbar">
-      <Row>
-        <Colxx xxs="7">
-          <Row className="h-100 align-items-center">
-            <Colxx xxs="2">
-              <Svg icon="DarkMode" />
-            </Colxx>
-            <Colxx xxs="10" className="d-flex align-items-center">
-              <div className="h1 mb-0">Acervo Livre</div>
+      <Row className="h-100 align-items-center">
+        <Colxx xxs="4">
+          <Svg icon="AutoStories" />
+        </Colxx>
+        <Colxx xxs="4">
+          <div className="h1 text-center">Acervo Livre</div>
+        </Colxx>
+        <Colxx xxs="4">
+          <Row>
+            <Colxx>teste</Colxx>
+            <Colxx>
+              <RcSwitch
+                checkedIcon="DarkMode"
+                uncheckedIcon="LightMode"
+                checked={switchTheme}
+                onChange={() => setSwitchTheme((o) => !o)}
+              />
             </Colxx>
           </Row>
-        </Colxx>
-        <Colxx
-          xxs="5"
-          className="d-flex align-items-center justify-content-end"
-        >
-          <RcSwitch
-            checkedIcon="DarkMode"
-            uncheckedIcon="LightMode"
-            checked={switchTheme}
-            onChange={() => setSwitchTheme((o) => !o)}
-          />
         </Colxx>
       </Row>
     </div>
